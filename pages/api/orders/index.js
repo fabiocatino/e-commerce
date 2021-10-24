ee83fff391535322e5a29a2f98571bacc13bc558
Fabio Catino <fabivs9@gmail.com>
2021-10-24 13:16:58 +0200

    order details

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -14,7 +14,6 @@ handler.get(async (req, res) => {
 	const user = await User.find({ email: session.user.email });
 	const _id = user[0]._id;
 	const orders = await Order.find({ user: _id }).sort({ createdAt: -1 });
-	// await db.disconnect();
 	res.send(orders);
 });
 
