9ed3d4248b2dd5ba1cae3ddc81dd473d55b284d4
Fabio Catino <fabivs9@gmail.com>
2021-10-25 20:19:16 +0200

    pagination done

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,6 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<title>E-commerce</title>
-					<meta
-						name="viewport"
-						content="initial-scale=1.0, width=device-width"
-					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
 						rel="stylesheet"
