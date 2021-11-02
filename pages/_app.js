01b384ee14fcc2d51bd3cfbea360eeefe5f46b1c
Fabio Catino <fabivs9@gmail.com>
2021-11-02 08:19:49 +0100

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -48,10 +48,11 @@ export default function App({
 	return (
 		<>
 			<Script
+				id="ga"
 				strategy="lazyOnLoad"
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
 			/>
-			<Script>
+			<Script id="ga1">
 				{`  window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
 					gtag('js', new Date());
